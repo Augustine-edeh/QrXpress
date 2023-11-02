497f8142a1536c6ad438db658b6ea75b861777de
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-11-02 23:59:36 +0100

    Add Main page

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Header from "./Components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className="w-screen h-screen bg-green-50 grid place-content-center bg-gradient-to-bl from-cyan-100 to-blue-200">
+        <div className=" flex flex-col space-y-5 md:flex-row md:space-y-0 md:space-x-6 lg:space-x-20">
+          <button
+            className="bg-blue-700 text-white py-3 px-6 rounded-lg hover:shadow-lg 
+        focus:opacity-[0.85]  hover:shadow-blue-300 focus:bg-blue-600"
+          >
+            Generate QR-code
+          </button>
+          <button className="bg-blue-800 text-white py-3 px-6 rounded-lg">
+            Scan QR-code
+          </button>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default App;
