0373a83c58743fa8223c3ed51ebadfc150051e02
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-12-11 22:53:31 +0100

    remove dead code

--- src/Components/UI/scannerUI/Scanner.jsx
+++ src/Components/UI/scannerUI/Scanner.jsx
@@ -3,40 +3,6 @@ import { Html5Qrcode } from "html5-qrcode";
 
 const Scanner = () => {
   useEffect(() => {
-    // This method will trigger user permissions
-    // Html5Qrcode.getCameras()
-    //   .then((devices) => {
-    //     /**
-    //      * devices would be an array of objects of type:
-    //      * { id: "id", label: "label" }
-    //      */
-    //     if (devices && devices.length) {
-    //       var cameraId = devices[0].id;
-    //       // .. use this to start scanning.
-    //       const html5QrCode = new Html5Qrcode("reader");
-    //       html5QrCode
-    //         .start(
-    //           cameraId,
-    //           {
-    //             fps: 10, // Optional, frame per seconds for qr code scanning
-    //             qrbox: { width: 250, height: 250 }, // Optional, if you want bounded box UI
-    //           },
-    //           (decodedText, decodedResult) => {
-    //             // do something when code is read
-    //           },
-    //           (errorMessage) => {
-    //             // parse error, ignore it.
-    //           }
-    //         )
-    //         .catch((err) => {
-    //           // Start failed, handle it.
-    //         });
-    //     }
-    //   })
-    //   .catch((err) => {
-    //     // handle err
-    //   });
-
     const html5QrCode = new Html5Qrcode("reader");
     const qrCodeSuccessCallback = (decodedText, decodedResult) => {
       /* handle success */
@@ -52,27 +18,11 @@ const Scanner = () => {
       config,
       qrCodeSuccessCallback
     );
-
-    // // Select front camera or fail with `OverconstrainedError`.
-    // html5QrCode.start(
-    //   { facingMode: { exact: "user" } },
-    //   config,
-    //   qrCodeSuccessCallback
-    // );
-
-    // // Select back camera or fail with `OverconstrainedError`.
-    // html5QrCode.start(
-    //   { facingMode: { exact: "environment" } },
-    //   config,
-    //   qrCodeSuccessCallback
-    // );
   }, []);
 
   return (
     <>
       <p>Scanner Page</p>
-      {/* <p>{html5QrcodeScanner.cameraScanImage.alt}</p>
-      <div>{console.log(html5QrcodeScanner)}</div> */}
       <div id="reader">dfghf</div>
     </>
   );
