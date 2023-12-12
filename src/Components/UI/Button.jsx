07197b4b1ebf429877368a6920931b39cd291eb1
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-12-12 16:54:02 +0100

    add funtionality & navigation to Button Component

--- src/Components/UI/Button.jsx
+++ src/Components/UI/Button.jsx
@@ -1,11 +1,13 @@
 import { useNavigate } from "react-router-dom";
-function Button({ children, route }) {
+function Button({ children, route, additionalStyles, onClick, onCreateQR }) {
   const navigate = useNavigate();
-  const navigateTo = () => navigate(route);
+  const navigateTo = () => {
+    onCreateQR();
+    navigate(route);
+  };
   return (
     <button
-      className="bg-blue-800 text-white py-3 px-6 rounded-lg hover:shadow-lg 
-focus:opacity-[0.85] hover:shadow-blue-300 focus:bg-blue-600"
+      className={`bg-blue-800 text-white py-3 px-6 rounded-lg hover:shadow-lg focus:opacity-[0.85] hover:shadow-blue-300 focus:bg-blue-600 ${additionalStyles}`}
       onClick={navigateTo}
     >
       {children}
