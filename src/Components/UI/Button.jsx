1218d33c6f48d0d8fbc9f817433cd6aa78eb541a
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-12-12 20:16:23 +0100

    bug fix(onCreateQR): check if onCreateQR is present as a prop 1st

--- src/Components/UI/Button.jsx
+++ src/Components/UI/Button.jsx
@@ -1,10 +1,16 @@
 import { useNavigate } from "react-router-dom";
 function Button({ children, route, additionalStyles, onClick, onCreateQR }) {
   const navigate = useNavigate();
+
   const navigateTo = () => {
-    onCreateQR();
+    // Check if onCreateQR is a function before calling it
+    if (typeof onCreateQR === "function") {
+      // Check if onCreateQR is passed a prop before calling it
+      onCreateQR();
+    }
     navigate(route);
   };
+
   return (
     <button
       className={`bg-blue-800 text-white py-3 px-6 rounded-lg hover:shadow-lg focus:opacity-[0.85] hover:shadow-blue-300 focus:bg-blue-600 ${additionalStyles}`}
