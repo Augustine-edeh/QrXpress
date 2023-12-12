817242fa59d0c370eced6301f3d488e83e3a9e2d
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-12-12 08:53:21 +0100

    remove redundant code

--- src/App.jsx
+++ src/App.jsx
@@ -3,26 +3,7 @@ import Header from "./Components/UI/Header";
 import Footer from "./Components/UI/Footer";
 import Home from "./Components/UI/Home";
 import Scanner from "./Components/UI/scannerUI/Scanner";
-// import Html5QrcodeScanner from "html5-qrcode";
 const App = () => {
-  function onScanSuccess(decodedText, decodedResult) {
-    // handle the scanned code as you like, for example:
-    console.log(`Code matched = ${decodedText}`, decodedResult);
-  }
-
-  function onScanFailure(error) {
-    // handle scan failure, usually better to ignore and keep scanning.
-    // for example:
-    console.warn(`Code scan error = ${error}`);
-  }
-
-  // let html5QrcodeScanner = new Html5QrcodeScanner(
-  //   "reader",
-  //   { fps: 10, qrbox: { width: 250, height: 250 } },
-  //   /* verbose= */ false
-  // );
-  // html5QrcodeScanner.render(onScanSuccess, onScanFailure);
-
   // || Checking if device is mobile
   // function isMobile() {
   //   const regex =
