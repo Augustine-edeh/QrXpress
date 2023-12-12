ad9a8d0226d3a5b28172418aa75d197b675b35c4
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-12-12 07:40:03 +0100

    handle scannSuccess

--- src/Components/UI/scannerUI/Scanner.jsx
+++ src/Components/UI/scannerUI/Scanner.jsx
@@ -4,20 +4,29 @@ import { Html5Qrcode } from "html5-qrcode";
 const Scanner = () => {
   useEffect(() => {
     const html5QrCode = new Html5Qrcode("reader");
-    const qrCodeSuccessCallback = (decodedText, decodedResult) => {
-      /* handle success */
-    };
     const config = { fps: 10, qrbox: { width: 250, height: 250 } };
 
+    const onScannSuccess = (decodedText, decodedResult) => {
+      /* handle success */
+      console.log("Text:", decodedText);
+      console.log("Result:", decodedResult);
+
+      //   Stop Scamming and remove Camera  UI
+      html5QrCode
+        .stop()
+        .then((ignore) => {
+          // QR Code scanning is stopped.
+        })
+        .catch((err) => {
+          // Stop failed, handle it.
+        });
+    };
+
     // If you want to prefer front camera
-    // html5QrCode.start({ facingMode: "user" }, config, qrCodeSuccessCallback);
+    // html5QrCode.start({ facingMode: "user" }, config, onScannSuccess);
 
     // If you want to prefer back camera
-    html5QrCode.start(
-      { facingMode: "environment" },
-      config,
-      qrCodeSuccessCallback
-    );
+    html5QrCode.start({ facingMode: "environment" }, config, onScannSuccess);
   }, []);
 
   return (
