a100b62561816f83e42296425fb8796b43ca6ae9
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-12-12 08:50:38 +0100

    remove dead code

--- src/Components/UI/Home.jsx
+++ src/Components/UI/Home.jsx
@@ -2,26 +2,11 @@ import { useNavigate } from "react-router-dom";
 import Button from "./Button";
 
 function Home() {
-  const navigate = useNavigate();
-  // const clickHandler = () => navigate("/scanQR");
-
   return (
     <>
       <div className="flex flex-col space-y-5 md:flex-row md:space-y-0 md:space-x-6 lg:space-x-20">
-        {/* <button
-          className="bg-blue-700 text-white py-3 px-6 rounded-lg hover:shadow-lg 
-        focus:opacity-[0.85]  hover:shadow-blue-300 focus:bg-blue-600"
-        >
-          Generate QR-code
-        </button> */}
         <Button route={"/scanQR"}>Scan QR-code</Button>
         <Button route={"/createQR"}>Create QR-code</Button>
-        {/* <button
-          className="bg-blue-800 text-white py-3 px-6 rounded-lg"
-          // onClick={clickHandler}
-        >
-          Scan QR-code
-        </button> */}
       </div>
     </>
   );
