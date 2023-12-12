c0397bc692a4b4065d62cc7bc7fd3b8d66e1857b
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-12-12 08:06:56 +0100

    add route for Scanner Component

--- src/Components/UI/Home.jsx
+++ src/Components/UI/Home.jsx
@@ -1,10 +1,16 @@
+import { useNavigate } from "react-router-dom";
+
 function Home() {
+  const navigate = useNavigate();
+  const clickHandler = () => navigate("/scanQR");
+
   return (
     <>
       <div className="flex flex-col space-y-5 md:flex-row md:space-y-0 md:space-x-6 lg:space-x-20">
         <button
           className="bg-blue-700 text-white py-3 px-6 rounded-lg hover:shadow-lg 
         focus:opacity-[0.85]  hover:shadow-blue-300 focus:bg-blue-600"
+          onClick={clickHandler}
         >
           Generate QR-code
         </button>
