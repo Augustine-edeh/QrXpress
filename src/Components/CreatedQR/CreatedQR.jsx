bebfc2b8341b99df2d526c5ed13884eef9a0284e
Augustine-edeh <info.augustinesedeh@gmail.com>
2023-12-22 11:50:58 +0100

    implement routing for CreateQR component and and text validation logic for enterend texts

--- src/Components/CreatedQR/CreatedQR.jsx
+++ src/Components/CreatedQR/CreatedQR.jsx
@@ -1,7 +1,24 @@
 import Button from "../UI/Button";
 const CreatedQR = () => {
   const file =
-    "https://api.qrserver.com/v1/create-qr-code/?data=HelloWorlssdhsdhfifd&amp;size=100x100";
+    "https://api.qrserver.com/v1/create-qr-code/?data=testing_My_QRcode_scanner_Projesct!!!&amp;size=100x100";
+
+  async function downloadHandler() {
+    // const file =
+    //   "https://api.qrserver.com/v1/create-qr-code/?data=testing_My_QRcode_scanner_Projesct&amp;size=100x100";
+    const image = await fetch(file);
+    const imageBlog = await image.blob();
+    const imageURL = URL.createObjectURL(imageBlog);
+
+    // console.log(imageURL);
+
+    const link = document.createElement("a");
+    link.href = imageURL;
+    link.download = "generated-QR";
+    document.body.appendChild(link);
+    link.click(() => alert("clicked!"));
+    document.body.removeChild(link);
+  }
   return (
     <>
       <div className="flex flex-col">
@@ -14,13 +31,15 @@ const CreatedQR = () => {
           Back
         </Button>
         <img src={file} alt="QR" title="" />
-        <button
-          href={file}
+        <a
+          onClick={downloadHandler}
+          // href={file}
           className="w-full bg-gray-100 border border-gray-500 mt-5 px-3 py-1"
-          download
+          // download="QR-image"
+          allow-downloads
         >
           Download QR
-        </button>
+        </a>
       </div>
     </>
   );
--- src/Components/pages/createQR/CreateQR.jsx
+++ src/Components/pages/createQR/CreateQR.jsx
@@ -1,25 +1,52 @@
 import Button from "../../UI/Button";
 import PageTitle from "../../UI/PageTitle";
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 const CreateQR = () => {
-  const enteredValueRef = useRef();
+  const [routeName, setRouteName] = useState("");
 
+  const condition = true;
+  const enteredValueRef = useRef();
+  const navigate = useNavigate();
+
+  // const goBack = () => navigate("/");
   const createQrHandler = () => {
     const enteredValue = enteredValueRef.current.value;
     // alert(enteredValue);
     // alert(
     //   "Sorry. The this feature has not been implemented.\nDevelopment is on going for this feature.\n\n\nPlease check back soon."
     // );
+    if (enteredValue.trim().length > 0) {
+      // alert(1);
+      if (condition === true) {
+        console.log("Yesterday!");
+        // navigate("/output");
+        setRouteName("/output"); // Save the route to state
+
+        // setRouteName("/output");
+      }
+      // update enteredValue context
+    } else {
+      alert("Enter valid text");
+      // display a passive (validation) error message UI 'Enter a valid text'
+    }
   };
+
+  useEffect(() => {
+    // Use the saved route from state to navigate
+    if (routeName) {
+      navigate(routeName);
+    }
+  }, [routeName, navigate]);
+
   return (
     <section className="">
       <PageTitle>Create Qr-Code</PageTitle>
       <div className="bg-blue-300 h-96 p-3 text-center flex flex-col place-content-center gap-2 rounded">
         <form
           className="h-5/6 grid place-content-center"
-          onSubmit={() => <div>Loading...</div>}
+          // onSubmit={() => <div>Loading...</div>}
         >
           <p className="text-left">
             <label htmlFor="input" className="font-bold text-xl">
@@ -38,9 +65,13 @@ const CreateQR = () => {
           {/* <input type="textarea" id="input" className="h-10" /> */}
         </form>
         <Button
-          route={"/output"}
-          additionalStyles={"h-1/6"}
+          // route={routeName}
           onCreateQR={createQrHandler}
+          additionalStyles={"h-1/6"}
+          // onClick={() => {
+          //   createQrHandler();
+          //   goBack();
+          // }}
         >
           Create QR
         </Button>
